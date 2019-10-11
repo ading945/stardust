a6ab5a67ee4d69b28b8453808de3c0076d9ad8bd
ading945 <53949977+ading945@users.noreply.github.com>
2019-10-10 20:57:48 -0700

    Create start.py
    creation of main

---
+++ start.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+"""
+Hello universe
+
+Copyright by alekding945
+"""
+
+import argparse
+import os
+
+
+
+
+if __name__ == '__main__':
+
+    # main
+    print("This is hello Universe!!")
